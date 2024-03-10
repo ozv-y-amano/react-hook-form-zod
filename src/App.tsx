7371f9d9aa0b1a1c9f25728b8f5209f5029ee22a
ozv-y-amano <y-amano@oz-vision.co.jp>
2024-03-10 11:10:20 +0900

    add: 従来のフォーム実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [name, setName] = useState("");
+	// メールアドレスの処理
+	// パスワードの処理
 
-	return <>Hello</>;
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+
+		console.log(name);
+	};
+	// メールアドレスの処理
+	// パスワードの処理
+	return (
+		<div className="form-container">
+			<h1>React-Hook-Form</h1>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<label htmlFor="名前">名前</label>
+				<input
+					id="name"
+					type="text"
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<label htmlFor="メールアドレス">メールアドレス</label>
+				<input id="email" type="email" />
+				<label htmlFor="パスワード">パスワード</label>
+				<input id="password" type="password" />
+
+				<button type="submit">送信</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
