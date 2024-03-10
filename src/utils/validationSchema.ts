c5a22549d01f05a95ecd9ed9349166721eae4bd6
ozv-y-amano <y-amano@oz-vision.co.jp>
2024-03-10 12:37:12 +0900

    mod: zod実装

---
+++ src/utils/validationSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const validationSchema = z.object({
+	name: z
+		.string()
+		.min(1, "名前は必須です")
+		.min(4, "名前は4文字以上で入力してください"),
+	email: z
+		.string()
+		.min(1, "メールアドレスは必須です")
+		.email("正しいメールアドレスを入力してください"),
+	password: z
+		.string()
+		.min(1, "パスワードは必須です")
+		.min(6, "パスワードは6文字以上で入力してください"),
+});
